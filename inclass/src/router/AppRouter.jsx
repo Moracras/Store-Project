8ab12c1a8268bd8834458c22beb5720e4b7d840e
Moracras <tavfikkarademir@gmail.com>
2023-12-23 12:28:32 +0300

    privaterouter

--- inclass/src/router/AppRouter.jsx
+++ inclass/src/router/AppRouter.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import  {Routes,Route} from "react-router-dom"
+import Login from "../pages/Login"
+import Navbar from "../components/Navbar"
+import Products from "../pages/Products"
+import ProductDetail from "../pages/ProductDetail"
+import About from "../pages/About"
+import NotFound from "../pages/NotFound"
+import Home from "../pages/Home"
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <>
+      <Navbar/>
+      <Routes>
+        
+        <Route path="/" element={<Login/>} />
+        <Route path='/dashboard' element={<PrivateRouter/>} >
+          <Route path='' element={<Home/>}/>
+          <Route path='/dashboard/products' element={<Products/>}/>
+          <Route path='products/:title' element={<ProductDetail/>}/>
+          <Route path='about' element={<About/>}/>
+        </Route>
+        <Route path="" element={<NotFound/>}/>
+
+      </Routes>
+    
+    </>
   )
 }
 

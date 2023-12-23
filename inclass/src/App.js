1836e4b0c00bacd40f5616026321c6d84d1ff591
Moracras <tavfikkarademir@gmail.com>
2023-12-23 13:52:02 +0300

    auth login navbar edit

--- inclass/src/App.js
+++ inclass/src/App.js
@@ -1,8 +1,11 @@
+import AuthProvider from "./context/AuthProvider";
 import Approuter from "./router/AppRouter"
 
 function App() {
   return (
+    <AuthProvider>
     <Approuter/>
+    </AuthProvider>
   );
 }
 
--- inclass/src/router/PrivateRouter.jsx
+++ inclass/src/router/PrivateRouter.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import {Navigate, Outlet} from "react-router-dom"
 import Navbar from '../components/Navbar'
+import { AuthContext } from '../context/AuthProvider'
+import { useContext } from 'react'
 
 const PrivateRouter = () => {
+  const {user} = useContext(AuthContext)
   return true? (
   <>
   <Navbar/>

3fce30ee5b80182963796365675192cdc3725958
Moracras <tavfikkarademir@gmail.com>
2023-12-25 20:42:01 +0300

    session storage

--- inclass/src/context/AuthProvider.jsx
+++ inclass/src/context/AuthProvider.jsx
@@ -1,11 +1,11 @@
 //AuthContext
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState(JSON.parse(sessionStorage.getItem("user")) || null);
   const navigate = useNavigate();
 
   const login = (info) => {
@@ -15,6 +15,10 @@ const AuthProvider = ({ children }) => {
   const logout = () =>{
     setUser(null)
   }
+
+  useEffect(()=>{
+    sessionStorage.setItem("user",JSON.stringify(user))
+  },[user])///! componentdidupdate
 
   return (
     <AuthContext.Provider value={{ user, login,logout }}>

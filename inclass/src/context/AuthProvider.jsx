757225ec2bd5d24b19a8cad0fd40923bed273e80
Moracras <tavfikkarademir@gmail.com>
2023-12-25 20:27:51 +0300

    private router fixed and logout onclick added

--- inclass/src/context/AuthProvider.jsx
+++ inclass/src/context/AuthProvider.jsx
@@ -12,9 +12,12 @@ const AuthProvider = ({ children }) => {
     setUser(info);
     navigate("/dashboard");
   };
+  const logout = () =>{
+    setUser(null)
+  }
 
   return (
-    <AuthContext.Provider value={{ user, login }}>
+    <AuthContext.Provider value={{ user, login,logout }}>
       {children}
     </AuthContext.Provider>
   );
